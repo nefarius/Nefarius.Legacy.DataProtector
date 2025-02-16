f2fbb3456a3ae89c0969d51d1711380b345a36a5
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2025-02-16 02:51:13 +0100

    Added files

---
+++ Nefarius.Legacy.DataProtector/DataProtection.cs
@@ -0,0 +1,42 @@
+﻿// ReSharper disable RedundantUsingDirective
+using System;
+using System.ComponentModel.DataAnnotations;
+#if NETFRAMEWORK
+using System.Data.Entity;
+// ReSharper disable UnusedType.Global
+#endif
+#if NETCOREAPP
+using Microsoft.EntityFrameworkCore;
+#endif
+
+namespace Nefarius.Legacy.DataProtector;
+
+public class DataProtectionKey
+{
+    [Key]
+    [MaxLength(200)]
+    public string Id { get; set; } = string.Empty;
+
+    public string XmlData { get; set; } = string.Empty;
+
+    public DateTime LastModified { get; set; } = DateTime.UtcNow;
+}
+
+#if NETFRAMEWORK
+public class DataProtectionDbContext : DbContext
+{
+    public DataProtectionDbContext(string connectionString) : base(connectionString) { }
+
+    public DbSet<DataProtectionKey> DataProtectionKeys { get; set; }
+}
+#endif
+
+#if NETCOREAPP
+public class DataProtectionDbContext : DbContext
+{
+    public DataProtectionDbContext(DbContextOptions<DataProtectionDbContext> options)
+        : base(options) { }
+
+    public DbSet<DataProtectionKey> DataProtectionKeys { get; set; }
+}
+#endif
---
+++ Nefarius.Legacy.DataProtector/EfXmlRepository.cs
@@ -0,0 +1,51 @@
+﻿// ReSharper disable UnusedType.Global
+#if NETFRAMEWORK
+using System.Data.Entity;
+#endif
+using System.Xml.Linq;
+using Microsoft.AspNetCore.DataProtection.Repositories;
+
+namespace Nefarius.Legacy.DataProtector;
+
+public class EfXmlRepository : IXmlRepository
+{
+    private readonly Func<DataProtectionDbContext> _contextFactory;
+
+    public EfXmlRepository(Func<DataProtectionDbContext> contextFactory)
+    {
+        _contextFactory = contextFactory ?? throw new ArgumentNullException(nameof(contextFactory));
+    }
+
+    public IReadOnlyCollection<XElement> GetAllElements()
+    {
+        using var context = _contextFactory();
+        return context.DataProtectionKeys
+            .Select(k => XElement.Parse(k.XmlData))
+            .ToList();
+    }
+
+    public void StoreElement(XElement element, string friendlyName)
+    {
+        using var context = _contextFactory();
+        var keyEntry = context.DataProtectionKeys.Find(friendlyName);
+
+        if (keyEntry == null)
+        {
+            keyEntry = new DataProtectionKey { Id = friendlyName, XmlData = element.ToString() };
+            context.DataProtectionKeys.Add(keyEntry);
+        }
+        else
+        {
+            keyEntry.XmlData = element.ToString();
+            keyEntry.LastModified = DateTime.UtcNow;
+
+#if NETFRAMEWORK
+            context.Entry(keyEntry).State = EntityState.Modified;
+#else
+            context.Entry(keyEntry).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+#endif
+        }
+
+        context.SaveChanges();
+    }
+}
