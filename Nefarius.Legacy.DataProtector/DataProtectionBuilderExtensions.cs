ee48ae3dc331d520611380af6692e07ab95bab07
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2025-02-16 15:41:28 +0100

    Added PersistKeysToSqlServer for .NET Framework

--- Nefarius.Legacy.DataProtector/DataProtectionBuilderExtensions.cs
+++ Nefarius.Legacy.DataProtector/DataProtectionBuilderExtensions.cs
@@ -6,6 +6,31 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Nefarius.Legacy.DataProtector;
 
+#if NETFRAMEWORK
+/// <summary>
+///     <see cref="IDataProtectionBuilder" /> extensions.
+/// </summary>
+[SuppressMessage("ReSharper", "UnusedType.Global")]
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+public static class DataProtectionBuilderExtensions
+{
+    /// <summary>
+    ///     Registers the <see cref="EfXmlRepository" /> using EF 6.
+    /// </summary>
+    public static IDataProtectionBuilder PersistKeysToSqlServer(this IDataProtectionBuilder builder,
+        string connectionString)
+    {
+        builder.AddKeyManagementOptions(options =>
+        {
+            options.XmlRepository = new EfXmlRepository(() => new DataProtectionDbContext(connectionString
+            ));
+        });
+
+        return builder;
+    }
+}
+#endif
+
 #if NETCOREAPP
 /// <summary>
 ///     <see cref="IDataProtectionBuilder"/> extensions.
