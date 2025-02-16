ab158de28536cbe178071fe6f579d978a0e47cf4
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2025-02-16 16:23:24 +0100

    Added optional ILogger support

--- Nefarius.Legacy.DataProtector/EfXmlRepository.cs
+++ Nefarius.Legacy.DataProtector/EfXmlRepository.cs
@@ -4,21 +4,25 @@ using System.Data.Entity;
 #endif
 using System.Xml.Linq;
 using Microsoft.AspNetCore.DataProtection.Repositories;
+using Microsoft.Extensions.Logging;
 
 namespace Nefarius.Legacy.DataProtector;
 
 public class EfXmlRepository : IXmlRepository
 {
     private readonly Func<DataProtectionDbContext> _contextFactory;
+    private readonly ILogger? _logger;
 
-    public EfXmlRepository(Func<DataProtectionDbContext> contextFactory)
+    public EfXmlRepository(Func<DataProtectionDbContext> contextFactory, ILoggerFactory? loggerFactory = null)
     {
         _contextFactory = contextFactory ?? throw new ArgumentNullException(nameof(contextFactory));
+        _logger = loggerFactory?.CreateLogger<EfXmlRepository>();
     }
 
     /// <inheritdoc />
     public IReadOnlyCollection<XElement> GetAllElements()
     {
+        _logger?.LogDebug("Getting all elements");
         using var context = _contextFactory();
         return context.DataProtectionKeys
             .Select(k => XElement.Parse(k.XmlData))
@@ -28,6 +32,8 @@ public class EfXmlRepository : IXmlRepository
     /// <inheritdoc />
     public void StoreElement(XElement element, string friendlyName)
     {
+        _logger?.LogDebug($"Storing element {friendlyName}.");
+        
         using var context = _contextFactory();
         var keyEntry = context.DataProtectionKeys.Find(friendlyName);
 
@@ -49,5 +55,7 @@ public class EfXmlRepository : IXmlRepository
         }
 
         context.SaveChanges();
+        
+        _logger?.LogDebug($"Stored element {friendlyName}.");
     }
 }
