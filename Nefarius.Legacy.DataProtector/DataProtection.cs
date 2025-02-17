c982b84fe8b173f2f65a2ad5f36448781f5b67c9
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2025-02-17 16:46:59 +0100

    Added missing XML docs

--- Nefarius.Legacy.DataProtector/DataProtection.cs
+++ Nefarius.Legacy.DataProtector/DataProtection.cs
@@ -13,32 +13,56 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Nefarius.Legacy.DataProtector;
 
+/// <summary>
+///     DB record that represents a stored data protection key.
+/// </summary>
 public class DataProtectionKey
 {
+    /// <summary>
+    ///     Primary key.
+    /// </summary>
     [Key]
     [MaxLength(200)]
     public string Id { get; set; } = string.Empty;
 
+    /// <summary>
+    ///     Key XML blob.
+    /// </summary>
     public string XmlData { get; set; } = string.Empty;
 
+    /// <summary>
+    ///     Last modified timestamp.
+    /// </summary>
     public DateTime LastModified { get; set; } = DateTime.UtcNow;
 }
 
 #if NETFRAMEWORK
+/// <summary>
+///     Data protection database context.
+/// </summary>
 public class DataProtectionDbContext : DbContext
 {
     public DataProtectionDbContext(string connectionString) : base(connectionString) { }
 
+    /// <summary>
+    ///     The key entries in the DB.
+    /// </summary>
     public DbSet<DataProtectionKey> DataProtectionKeys { get; set; }
 }
 #endif
 
 #if NETCOREAPP
+/// <summary>
+///     Data protection database context.
+/// </summary>
 public class DataProtectionDbContext : DbContext
 {
     public DataProtectionDbContext(DbContextOptions<DataProtectionDbContext> options)
         : base(options) { }
 
+    /// <summary>
+    ///     The key entries in the DB.
+    /// </summary>
     public DbSet<DataProtectionKey> DataProtectionKeys { get; set; }
 }
 #endif
