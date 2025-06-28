0b25bc627996dc89f98142cd362a603a51a46685
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2025-06-28 22:25:16 +0200

    Update EnsureDataProtectionKeysTableStartupFilter.cs

--- src/EnsureDataProtectionKeysTableStartupFilter.cs
+++ src/EnsureDataProtectionKeysTableStartupFilter.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
 namespace Nefarius.Legacy.DataProtector;
@@ -14,40 +15,49 @@ internal sealed class EnsureDataProtectionKeysTableStartupFilterOptions
 }
 
 internal sealed class EnsureDataProtectionKeysTableStartupFilter(
-    IConfiguration configuration,
-    IOptions<EnsureDataProtectionKeysTableStartupFilterOptions> options) : IStartupFilter
+    IServiceProvider serviceProvider) : IStartupFilter
 {
     /// <inheritdoc />
     public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
     {
-        EnsureTableExists();
-        return next;
-    }
+        return app =>
+        {
+            using IServiceScope scope = serviceProvider.CreateScope();
+            IConfiguration? configuration = scope.ServiceProvider.GetService<IConfiguration>();
+            IOptions<EnsureDataProtectionKeysTableStartupFilterOptions>? options = scope.ServiceProvider
+                .GetService<IOptions<EnsureDataProtectionKeysTableStartupFilterOptions>>();
 
-    private void EnsureTableExists()
-    {
-        string? connectionString = configuration.GetConnectionString(options.Value.ConnectionStringName);
+            if (options is null || configuration is null)
+            {
+                next(app);
+                return;
+            }
 
-        ArgumentException.ThrowIfNullOrEmpty(connectionString);
-        
-        using SqlConnection connection = new(connectionString);
-        connection.Open();
+            string? connectionString = configuration.GetConnectionString(options.Value.ConnectionStringName);
 
-        using SqlCommand command = connection.CreateCommand();
-        command.CommandText = """
-                                  IF NOT EXISTS (
-                                      SELECT * FROM INFORMATION_SCHEMA.TABLES 
-                                      WHERE TABLE_NAME = 'DataProtectionKeys' AND TABLE_SCHEMA = 'dbo'
-                                  )
-                                  BEGIN
-                                      CREATE TABLE [dbo].[DataProtectionKeys] (
-                                          [Id] INT IDENTITY PRIMARY KEY,
-                                          [FriendlyName] NVARCHAR(256) NOT NULL,
-                                          [Xml] NVARCHAR(MAX) NOT NULL
+            ArgumentException.ThrowIfNullOrEmpty(connectionString);
+
+            using SqlConnection connection = new(connectionString);
+            connection.Open();
+
+            using SqlCommand command = connection.CreateCommand();
+            command.CommandText = """
+                                      IF NOT EXISTS (
+                                          SELECT * FROM INFORMATION_SCHEMA.TABLES 
+                                          WHERE TABLE_NAME = 'DataProtectionKeys' AND TABLE_SCHEMA = 'dbo'
                                       )
-                                  END
-                              """;
-        command.ExecuteNonQuery();
+                                      BEGIN
+                                          CREATE TABLE [dbo].[DataProtectionKeys] (
+                                              [Id] INT IDENTITY PRIMARY KEY,
+                                              [FriendlyName] NVARCHAR(256) NOT NULL,
+                                              [Xml] NVARCHAR(MAX) NOT NULL
+                                          )
+                                      END
+                                  """;
+            command.ExecuteNonQuery();
+
+            next(app);
+        };
     }
 }
 #endif
