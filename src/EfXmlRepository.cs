49282d0b6a4d9965690525b53170d8b4ee681c02
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2025-06-28 20:34:31 +0200

    Added EnsureDataProtectionKeysTableExists

--- src/EfXmlRepository.cs
+++ src/EfXmlRepository.cs
@@ -1,5 +1,11 @@
 ﻿// ReSharper disable UnusedType.Global
 
+
+#if NETCOREAPP
+using Microsoft.EntityFrameworkCore;
+#endif
+
+using System.Data.Common;
 using System.Xml.Linq;
 
 using Microsoft.AspNetCore.DataProtection.Repositories;
@@ -29,6 +35,9 @@ internal class EfXmlRepository : IXmlRepository
     {
         _logger?.LogDebug("Getting all elements");
         using DataProtectionDbContext? context = _contextFactory();
+
+        EnsureDataProtectionKeysTableExists(context);
+
         return context.DataProtectionKeys
             .Select(k => k.XmlData)
             .ToList()
@@ -42,6 +51,9 @@ internal class EfXmlRepository : IXmlRepository
         _logger?.LogDebug("Storing element {FriendlyName}.", friendlyName);
 
         using DataProtectionDbContext? context = _contextFactory();
+
+        EnsureDataProtectionKeysTableExists(context);
+
         DataProtectionKey? keyEntry = context.DataProtectionKeys.Find(friendlyName);
 
         if (keyEntry == null)
@@ -57,12 +69,40 @@ internal class EfXmlRepository : IXmlRepository
 #if NETFRAMEWORK
             context.Entry(keyEntry).State = EntityState.Modified;
 #else
-            context.Entry(keyEntry).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            context.Entry(keyEntry).State = EntityState.Modified;
 #endif
         }
 
         context.SaveChanges();
 
         _logger?.LogDebug("Stored element {FriendlyName}.", friendlyName);
+    }
+
+    private static void EnsureDataProtectionKeysTableExists(DataProtectionDbContext context)
+    {
+        const string createTableSql = """
+
+                                      IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'DataProtectionKeys')
+                                      BEGIN
+                                          CREATE TABLE [DataProtectionKeys] (
+                                              [Id] int NOT NULL PRIMARY KEY IDENTITY,
+                                              [FriendlyName] nvarchar(256) NOT NULL,
+                                              [Xml] nvarchar(max) NOT NULL
+                                          )
+                                      END
+                                      """;
+
+        try
+        {
+#if NETFRAMEWORK
+            context.Database.ExecuteSqlCommand(createTableSql);
+#else
+            context.Database.ExecuteSqlRaw(createTableSql);
+#endif
+        }
+        catch (DbException)
+        {
+            // ignored
+        }
     }
 }
